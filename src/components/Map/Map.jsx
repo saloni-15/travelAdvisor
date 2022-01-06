6b3885b8cd09274b51d73b7ef873486df0fdbb69
Saloni <subedisaloni2002@gmail.com>
2022-01-06 22:26:25 +0530

    ✨map component

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab";
+
+import useStyles from "./styles";
 
 const Map = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(min-width:600px)");
+
+  const coordinates = { lat: 0, lon: 0 };
+
   return (
-    <div>
-      <h1>Map component.</h1>
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onchange={""}
+        onChildClick={""}
+      ></GoogleMapReact>
     </div>
   );
 };
