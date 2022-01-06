81a16eaa1bb0e981f9a26896b7d4f9b832e7cfc1
Saloni <subedisaloni2002@gmail.com>
2022-01-06 21:35:33 +0530

    ✨Header component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {CssBaseline, Grid} from "@material-ui/core"
 import Header from "./components/Header/Header";
 import List from "./components/List/List";
 import Map from "./components/Map/Map";
-import PlaceDetails from "./components/PlaceDetails/PlaceDetails";
 
 const App = () => {
   return (
@@ -23,3 +22,4 @@ const App = () => {
 };
 
 export default App;
+ 
