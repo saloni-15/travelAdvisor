cdb4fcd25454b0366bc2b05db413f088f521d41a
Saloni <subedisaloni2002@gmail.com>
2022-01-07 10:27:55 +0530

    ✨implement google-map-react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {CssBaseline, Grid} from "@material-ui/core"
+import { CssBaseline, Grid } from "@material-ui/core";
 import Header from "./components/Header/Header";
 import List from "./components/List/List";
 import Map from "./components/Map/Map";
@@ -22,4 +22,3 @@ const App = () => {
 };
 
 export default App;
- 
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
 import { Paper, Typography, useMediaQuery } from "@material-ui/core";
-import LocationOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
 import Rating from "@material-ui/lab";
 
 import useStyles from "./styles";
@@ -10,15 +10,15 @@ const Map = () => {
   const classes = useStyles();
   const isMobile = useMediaQuery("(min-width:600px)");
 
-  const coordinates = { lat: 0, lon: 0 };
+  const coordinates = { lat: 20.5937, lng: 78.9629 };
 
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: "" }}
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API }}
         defaultCenter={coordinates}
         center={coordinates}
-        defaultZoom={14}
+        defaultZoom={5}
         margin={[50, 50, 50, 50]}
         options={""}
         onchange={""}
