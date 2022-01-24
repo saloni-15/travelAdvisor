090f5b97730f8234a53f5bad8b2fbb013d3f6c9b
Saloni <subedisaloni2002@gmail.com>
2022-01-24 18:16:00 +0530

    ✨implement google-map-api and travel-advisor-api

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -2,15 +2,13 @@ import React from "react";
 import GoogleMapReact from "google-map-react";
 import { Paper, Typography, useMediaQuery } from "@material-ui/core";
 import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
-import Rating from "@material-ui/lab";
+import Rating from "@material-ui/lab/Rating";
 
 import useStyles from "./styles";
 
-const Map = () => {
+const Map = ({ setCoordinates, setBounds, coordinates, places }) => {
   const classes = useStyles();
-  const isMobile = useMediaQuery("(min-width:600px)");
-
-  const coordinates = { lat: 20.5937, lng: 78.9629 };
+  const isDesktop = useMediaQuery("(min-width:600px)");
 
   return (
     <div className={classes.mapContainer}>
@@ -18,12 +16,53 @@ const Map = () => {
         bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API }}
         defaultCenter={coordinates}
         center={coordinates}
-        defaultZoom={5}
+        defaultZoom={14}
         margin={[50, 50, 50, 50]}
         options={""}
-        onchange={""}
+        onChange={(e) => {
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ sw: e.marginBounds.sw, ne: e.marginBounds.ne });
+        }}
         onChildClick={""}
-      ></GoogleMapReact>
+      >
+        {places?.map((place, i) => (
+          <div
+            className={classes.markerContainer}
+            lat={Number(place.latitude)}
+            lng={Number(place.longitude)}
+            key={i}
+          >
+            {!isDesktop ? (
+              <LocationOnOutlinedIcon color="primary" fontSize="large" />
+            ) : (
+              <Paper elevation={3} className={classes.paper}>
+                <Typography
+                  className={classes.typography}
+                  variant="subtitle2"
+                  gutterBottom
+                >
+                  {" "}
+                  {place.name}
+                </Typography>
+                <img
+                  className={classes.pointer}
+                  src={
+                    place.photo
+                      ? place.photo.images.large.url
+                      : "https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg"
+                  }
+                />
+                <Rating
+                  name="read-only"
+                  size="small"
+                  value={Number(place.rating)}
+                  readOnly
+                />
+              </Paper>
+            )}
+          </div>
+        ))}
+      </GoogleMapReact>
     </div>
   );
 };
